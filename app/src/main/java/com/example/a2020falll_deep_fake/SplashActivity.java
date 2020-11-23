d97c003e72e373664621eebfe3e8f90cfb533d00
Jaemin Cho <jjm7307@kaist.ac.kr>
2020-11-24 02:02:36 +0900

    first upload

--- app/src/main/java/com/example/a2020falll_deep_fake/SplashActivity.java
+++ app/src/main/java/com/example/a2020falll_deep_fake/SplashActivity.java
@@ -1,4 +1,64 @@
 package com.example.a2020falll_deep_fake;
 
-public class SplashActivity {
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.core.app.ActivityCompat;
+
+public class SplashActivity extends AppCompatActivity{
+    String[] PERMISSIONS = {
+            //List for get permission
+            Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.ACCESS_COARSE_LOCATION,
+            Manifest.permission.INTERNET
+    };
+
+    private ConstraintLayout screen;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        askPermission();
+        while(!hasPermissions(this,PERMISSIONS)){}
+
+        screen = (ConstraintLayout)findViewById(R.id.screen);
+
+        screen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(SplashActivity.this, "조재민(님)으로 로그인하셨습니다.", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+
+            }
+        });
+    }
+
+    private void askPermission() {
+        int PERMISSION_ALL = 1;
+
+        if (!hasPermissions(this, PERMISSIONS)) {
+            ActivityCompat.requestPermissions(this, PERMISSIONS, PERMISSION_ALL);
+        }
+    }
+    public static boolean hasPermissions(Context context, String... permissions) {
+        if (context != null && permissions != null) {
+            for (String permission : permissions) {
+                if (ActivityCompat.checkSelfPermission(context, permission) != PackageManager.PERMISSION_GRANTED) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
 }
